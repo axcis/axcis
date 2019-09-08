33795ec062df3761bbe11c0bebb555bf4474cfc7
gozuchi <gozuchi@LAPTOP-SLA9MUOG>
2019-09-09 08:25:20 +0900

    axcisコーポレートサイト作成

---
+++ _js/functions.js
@@ -0,0 +1,77 @@
+/*
+$(function() {
+    var nav = $('nav');
+    //表示位置
+    var navTop = nav.offset().top+100;
+    //ナビゲーションの高さ（シャドウの分だけ足してます）
+    var navHeight = nav.height()+10;
+    var showFlag = false;
+    nav.css('top', -navHeight+'px');
+    //ナビゲーションの位置まできたら表示
+    $(window).scroll(function () {
+        var winTop = $(this).scrollTop();
+        if (winTop >= navTop) {
+            if (showFlag == false) {
+                showFlag = true;
+                nav
+                    .addClass('fixed')
+                    .stop().animate({'top' : '0px'}, 300);
+            }
+        } else if (winTop <= navTop) {
+            if (showFlag) {
+                showFlag = false;
+                nav.stop().animate({'top' : -navHeight+'px'}, 300, function(){
+                    nav.removeClass('fixed');
+                });
+            }
+        }
+    });
+});
+*/
+$(function(){
+var pageTop=$('#pageTop');
+pageTop.hide();
+ 
+//◇ボタンの表示設定
+$(window).scroll(function(){
+  if($(this).scrollTop()>100){
+    //---- 画面を80pxスクロールしたら、ボタンを表示する
+    pageTop.fadeIn();
+  }else{
+    //---- 画面が80pxより上なら、ボタンを表示しない
+    pageTop.fadeOut();
+  } 
+});
+ 
+// ◇ボタンをクリックしたら、スクロールして上に戻る
+pageTop.click(function(){
+  $('body,html').animate({
+  scrollTop: 0},300);
+  return false;
+});
+
+});
+
+$(function() {
+     $("#menu-trigger").click(function() {
+           $("#overlay").fadeIn();
+ });
+     $("#close,#overlay li").click(function() {
+           $("#overlay").fadeOut();
+ });
+});
+
+/*
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
+*/
---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>株式会社アクシス｜SES システムエンジニアリングサービス</title>
+	<meta name="description" content="株式会社アクシスは東京都でSES事業を中心とし、システムの受託開発やWEBコンテンツ制作事業を行っております。確かな技術力と高い人間力を重要視し、成果に繋がるサービスをご提案致します。">
+	<meta name="keywords" content="アクシス,SES,システム開発,IT,WEB制作">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="format-detection" content="telephone=no">
+	<link rel="icon" href="_images/favicon.png">
+	<link rel="apple-touch-icon" href="" />
+	<link rel="stylesheet" href="_css/html5reset-1.6.1.css">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="_css/style.css">
+	<link href="https://fonts.googleapis.com/earlyaccess/sawarabimincho.css" rel="stylesheet" />
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+	<script src="_js/jquery.validate.min.js"></script>
+	<script>
+		if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
+		  $(function() {
+		    $('.tel').each(function() {
+		      var str = $(this).html();
+		      if ($(this).children().is('img')) {
+		        $(this).html($('<a>').attr('href', 'tel:' + $(this).children().attr('alt').replace(/-/g, '')).append(str + '</a>'));
+		      } else {
+		        $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
+		      }
+	    });
+	  });
+	}
+	</script>
+	<script type="text/javascript" src="_js/jquery.matchHeight.js"></script>
+	<script>
+	$(function(){
+	$('#business-list dl').matchHeight();
+	});
+	</script>
+	<script src="_js/functions.js"></script>
+</head>
+<body>
+	<div id="mv">
+		<div id="mv-contents">
+		<img src="_images/logo.png" alt="株式会社アクシス">
+		<h1 class="ja">人と人を繋ぐ未来へ</h1>
+		</div>
+		<p id="menu-trigger" class="pc-none ja"><i class="fa fa-bars"></i></p>
+		<div id="overlay" class="ja">
+		<p id="close"><i class="fa fa-times mar5" aria-hidden="true"></i>CLOSE</p>
+		<ul>
+			<li><a href="#top-msg">AXCIS</a></li>
+			<li><a href="#business">事業内容</a></li>
+			<li><a href="#corporate">会社概要</a></li>
+			<li><a href="#contact">お問い合わせ</a></li>
+		</ul>
+		</div>
+	</div>
+	<nav class="pc-only">
+		<ul class="cf ja">
+			<li><a href="#top-msg">AXCIS</a></li>
+			<li><a href="#business">事業内容</a></li>
+			<li><a href="#corporate">会社概要</a></li>
+			<li><a href="#contact">お問い合わせ</a></li>
+		</ul>
+	</nav>
+	<div id="top-msg" class="contents-wrap">
+		<h2 class="ja"><span>AXCIS</span><br>－人と人を繋ぐ未来へ－</h2>
+		<div class="contents-msg cf">
+		<p>株式会社アクシスは、SES事業を中心とし、システムの受託開発やWEBコンテンツ制作事業を行っております。弊社では最高のパフォーマンスを発揮するためには、確かな技術力だけではなく、高い人間力が重要だと考えています。お客様とエンジニア双方の想いを大切に、事業の活性化や作業コスト・リスクの大幅な軽減等、成果に繋がるサービスをご提案致します。</p>
+		<img src="_images/motto.png" alt="奉仕性・発展性・専門性">
+		</div>
+	</div>
+	<div id="business" class="contents-wrap">
+		<h2 class="ja"><span>BUSINESS</span><br>－事業内容－</h2>
+		<div id="business-list" class="cf">
+			<dl>
+				<dt class="ja">SES事業</dt>
+				<img src="_images/business01.png" alt="システムエンジニアリングサービス事業">
+				<dd>システムエンジニアリングサービスを展開し、開発業務をご提供することで、事業の活性化と作業コスト・リスクを大幅に軽減することに貢献致します。</dd>
+			</dl>
+			<dl>
+				<dt class="ja">受託開発事業</dt>
+				<img src="_images/business02.png" alt="受託開発事業">
+				<dd>お客様のお悩みや目的に応じ、システムやWEBコンテンツ全体の企画提案から行っております。各種システムの開発やWEBコンテンツの制作等、様々なご要望をお伺いした上で、最適なプランをご提案致します。</dd>
+			</dl>
+			<dl>
+				<dt class="ja">ビジネスパートナー</dt>
+				<img src="_images/business03.png" alt="ビジネスパートナー募集">
+				<dd>SES事業、受託開発事業（システム開発・WEBコンテンツ）において、ご協力頂けるパートナー会社・協力会社様を募集しております。</dd>
+			</dl>
+		</div>
+	</div>
+	<div id="corporate" class="contents-wrap">
+		<h2 class="ja"><span>CORPORATE</span><br>－会社概要－</h2>
+		<div id="corporate-list" class="cf">
+			<dl>
+				<dt>会社名</dt>
+				<dd>株式会社アクシス</dd>
+				<dt>設立</dt>
+				<dd>XXXX年X月XX日</dd>
+				<dt>資本金</dt>
+				<dd>XXXX万円</dd>
+				<dt>所在地</dt>
+				<dd>〒000-0000 東京都</dd>
+				<dt>電話番号</dt>
+				<dd>XXXX</dd>
+				<dt>FAX</dt>
+				<dd>XXXX</dd>
+				<dt>事業内容</dt>
+				<dd class="lineh">SES事業<br>
+				受託開発事業<br>
+				</dd>
+				<dt>取引銀行</dt>
+				<dd>XXXX</dd>
+<!-- 				<dt>個人情報保護方針</dt>
+				<dd class="privacy-link"><a href="privacy.php">プライバシーポリシー<i class="fa fa-external-link a-link-mark" aria-hidden="true"></i></a></dd> -->
+			</dl>
+		</div>
+	</div>
+	<div id="contact" class="contents-wrap">
+		<h2 class="ja"><span>CONTACT</span><br>－お問い合わせ－</h2>
+		<div id="corporate-list" class="cf">
+			<p style="text-align: center; font-size: 120%;">大変恐れ入りますが、只今準備中です。</p>
+			<!-- <form action="send.php" method="post">
+			<dl>
+				<dt>お名前<span class="req">必須</span></dt>
+				<dd><input type="text" name="name" autocomplete="name"></dd>
+				<dt>ふりがな<span class="req">必須</span></dt>
+				<dd><input type="text" name="kana" autocomplete="name"></dd>
+				<dt>メールアドレス<span class="req">必須</span></dt>
+				<dd><input type="email" name="email" autocomplete="email"></dd>
+				<dt>電話番号</dt>
+				<dd><input type="tel"  name="tel" autocomplete="tel"></dd>
+				<dt>お問い合わせ内容<span class="req">必須</span></dt>
+				<dd><textarea name="comment" id="" cols="30" rows="10" wrap="hard"></textarea></dd>
+				<dt>個人情報の取り扱いについて<span class="req">必須</span></dt>
+				<dd class="bb0">
+					<input type="checkbox" name="ag" id="checkbox" />
+					<label for="checkbox">下記の個人情報の取り扱いに同意する</label>
+				<div id="error"></div>
+				</dd>
+				<dd>
+					<div id="privacy-area">
+					<p class="mb2em">このフォームで頂戴する個人情報の取り扱いについて</p>
+					<p>1.事業者の氏名又は名称</p>
+					<p>株式会社アクシス</p>
+					<p>2.個人情報保護管理者（若しくはその代理人）の氏名又は職名、所属及び連絡先</p>
+					<ul>
+					<li>管理者名:今井</li>
+					<li>所属部署:SES システムエンジニアリングサービス事業部</li>
+					<li>連絡先:TEL:027-289-3209</li>
+					</ul>
+					<p>3.個人情報の利用目的</p>
+					<p>・採用応募者並びにそれに準ずる方の個人情報は、弊社の人事採用活動（書類審査、面接、評価、応募者への連絡等）のために利用いたします。</p>
+					<p>4.個人情報取扱いの委託</p>
+					<p>当社は事業運営上、前項利用目的の範囲に限って個人情報を外部に委託することがあります。この場合、個人情報保護水準の高い委託先を選定し、個人情報の適正管理・機密保持についての契約を交わし、適切な管理を実施させます。</p>
+					<p>5.個人情報の開示等の請求</p>
+					<p>ご本人様は、当社に対してご自身の個人情報の開示等に関して、下記の当社問合わせ窓口に申し出ることができます。その際、当社はお客様ご本人を確認させていただいたうえで、合理的な期間内に対応いたします。</p>
+					<p>【お問合せ窓口】</p>
+					<ul>
+					<li>株式会社アクシス 個人情報問合せ係</li>
+					<li>〒371-0843 群馬県前橋市新前橋町18番地29 Ｋビル1階</li>
+					<li>TEL:027-289-3209 （受付時間 10:00～18:00※）</li>
+					<li>※ 土・日曜日、祝日、年末年始、ゴールデンウィーク期間は翌営業日以降の対応とさせていただきます。</li>
+					</ul>
+					<p>6.個人情報を提供されることの任意性について</p>
+					<p>ご本人様が当社に個人情報を提供されるかどうかは任意によるものです。ただし、必要な項目をいただけない場合、適切な対応ができない場合があります。</p>
+					</div>
+				</dd>
+				<p class="privacy-link">ご入力頂いた個人情報は、上記の利用目的の達成に必要な範囲を超えた個人情報の取扱い（目的外利用）を行いません。また、目的外利用を行わないための措置を講じます。当社での個人情報の取り扱いについて、詳しくは<a href="privacy.php">プライバシーポリシー<i class="fa fa-external-link a-link-mark" aria-hidden="true"></i></a>をご確認下さい。</p>
+				<div id="btn-entry">
+				<button type="submit">入力内容を確認した上で送信する</button>
+				</div>
+			</dl>
+			</form> -->
+		</div>
+	</div>
+	<footer>
+	<p>Copyright (c) 株式会社AXCIS All Rights Reserved.</p>		
+	</footer>
+	<p id="pageTop"><a href=""><img src="_images/topicon.png"></a></p>
+</body>
+</html>
